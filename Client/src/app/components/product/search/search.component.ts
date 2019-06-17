acd7fdbc35d650afe852364d86a9e0ef61ce4ce2
Abdelrady Tantawy <abdelrady@gmail.com>
2019-06-17 18:08:54 -0500

    Add missing files

---
+++ Client/src/app/components/product/search/search.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from 'src/app/products.service';
+import { Product } from 'src/app/Product';
+import { environment } from '../../../../environments/environment';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: '../../../list-products/list-products.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  products : Product[] = [];
+  serverImagesUrl : String = environment.apiUrl + "/images/";
+
+  constructor(private route : ActivatedRoute, private productsService : ProductsService) {
+    route.queryParams.subscribe(query=>{
+        let searchTerm = query['q'];
+        this.productsService.searchProducts(searchTerm)
+        .subscribe((data : any) => {
+          if(data.success){
+            this.products = data.products;
+          }else{
+            // show error message
+          }
+        });;
+    })
+   }
+
+  ngOnInit() {
+  }
+
+}
