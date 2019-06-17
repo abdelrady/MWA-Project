12b5523076f0268a72c55e4cea0c58ad7b6791a9
zhangziyi <zhangziyiemail@gmail.com>
2019-06-16 21:02:59 -0500

    add

---
+++ Client/src/app/product.ts
@@ -0,0 +1,11 @@
+export class product {
+    _id?: Object;
+    // tags: number;
+    name: string;
+    description?: string;
+    quantity?: number;
+    price?: number;
+    imageId: string;
+    company: string;
+
+}
