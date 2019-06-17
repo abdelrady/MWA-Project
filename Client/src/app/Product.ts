706e32187cb218595686a78bb2add7b2e4395807
Abdelrady Tantawy <abdelrady@gmail.com>
2019-06-16 20:34:58 -0500

    Fix minor issues in front-end

--- Client/src/app/Product.ts
+++ Client/src/app/Product.ts
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 export class Product {
-    _id: Object;
-    tags: number;
-=======
-export class product {
-    //_id: Object;
+    _id?: Object;
     // tags: number;
->>>>>>> 361d46e8efe2e52899a1ce8523b1834dcbd5eeb4
     name: string;
-        description: string;
-      quantity: number;
-        price: number;
+    description?: string;
+    quantity?: number;
+    price?: number;
     imageId: string;
     company: string;
 
