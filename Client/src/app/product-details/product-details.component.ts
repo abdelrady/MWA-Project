db1bccd7ba12d5be6187c3b0d8b0520af60ec234
Efremsd <efremdebesay25@gmail.com>
2019-06-17 17:57:09 -0500

    change on list-product && product-details

--- Client/src/app/product-details/product-details.component.ts
+++ Client/src/app/product-details/product-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product';
+import { environment } from 'src/environments/environment';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product-details',
@@ -7,13 +10,27 @@ import { Product } from '../product';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-  private mobile: Product = {
-    name: "Iphone",
-    company: "Apple",
-    imageId: "bb76"
+  productId;
+  product: Product = { name: '', company: '', imageId: null };
+  serverImagesUrl: String = environment.apiUrl + "/images/";
+
+  constructor(private route: ActivatedRoute, private productService: ProductsService) {
+    route.params.subscribe(p => {
+      this.productId = p['id'];
+      productService.getProductById(this.productId)
+        .subscribe((data: any) => {
+          // Bind data to UI
+          if (data.success) {
+            this.product = data.product;
+          }
+          else {
+            // show error in UI
+          }
+        });
+    });
   }
 
-  constructor() { }
+
 
   ngOnInit() {
   }
