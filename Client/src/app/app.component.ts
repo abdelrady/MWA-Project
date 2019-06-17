c4cf40166bb89e3d9414f0e1280e657fcca0bf59
zhangziyi <zhangziyiemail@gmail.com>
2019-06-17 13:57:33 -0500

    commit

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,24 +1,36 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit } from '@angular/core';
 import { UsersService } from './users.service';
 import { FormBuilder } from '@angular/forms';
+import {TokenService} from './token.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Client';
   searchForm;
 
-  constructor(private formBuilder: FormBuilder, private usersService : UsersService){
+  constructor(private formBuilder: FormBuilder, private tokenService : TokenService){
     this.searchForm = formBuilder.group({
       'search': ['']
     });
-    
+
+  }
+
+  ngOnInit(){
+    const  token = this.tokenService.getUserInfo;
+    if(token){
+
+    }else{
+
+    }
   }
 
   search(){
-    
+
   }
+
+
 }
