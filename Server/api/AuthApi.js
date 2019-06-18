ea8645d5c3b95aa9ddfa0e9a53b4f3015ec008bf
Abdelrady Tantawy <abdelrady@gmail.com>
2019-06-18 04:20:51 -0500

    Fix login/register after switching to bcrypt-nodejs

--- Server/api/AuthApi.js
+++ Server/api/AuthApi.js
@@ -8,34 +8,42 @@ const saltRounds = 10;
 //http://travistidwell.com/jsencrypt/demo/
 let privateKey = fs.readFileSync("keys-private.pem", 'utf8');
 
-router.post("/signup", async (req, res, next) => {
+router.post("/signup", (req, res, next) => {
     // TODO make sure password & confirm-password are equal
-    const hash = await bcrypt.hash(req.body.password, saltRounds);
-    console.log(req.body.firstName);
-    let user = new User({
-        password: hash,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        isAdmin: req.body.isAdmin,
-        cartItems: []
+    bcrypt.hash(req.body.password, null, null, async(err, hash)=>{
+        if(err){
+            res.status(500).json({ success: false, err }).end();
+        }else{
+            let user = new User({
+                password: hash,
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                isAdmin: req.body.isAdmin,
+                cartItems: []
+            });
+            await user.save();
+            res.status(201).json({ success: true, user }).end();
+        }
     });
-    await user.save();
-    res.status(201).json({ success: true, user }).end();
 });
 
 router.post("/login", async (req, res, next) => {
     let user = await User.findOne({ "email": req.body.email });
     if (req.body.email && req.body.password && user) {
         const { cartItems, password, ...publicUser } = user._doc;
-        const isValid = await bcrypt.compare(req.body.password, password);
-        if (!isValid) res.json({ success: false });
-        var signOptions = {
-            algorithm: "RS256"
-        };
-        jwt.sign(publicUser, privateKey, signOptions, function (err, token) {
-            console.log(err);
-            res.json({ success: !err, token: token });
+        bcrypt.compare(req.body.password, password, (error, isValid) => {
+            console.log("isValid: " + isValid)
+            if (!isValid) res.json({ success: false });
+            else{
+                var signOptions = {
+                    algorithm: "RS256"
+                };
+                jwt.sign(publicUser, privateKey, signOptions, function (err, token) {
+                    console.log(err);
+                    res.json({ success: !err, token: token });
+                });
+            }
         });
     } else {
         res.json({ success: false });
@@ -43,7 +51,7 @@ router.post("/login", async (req, res, next) => {
 });
 
 // Global error handler for this module
-router.use("*", (req, res, next)=>{
+router.use("*", (req, res, next) => {
     res.status(500).json({ success: false });
 })
 
